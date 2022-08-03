798100d9e6e52d7c192c1a1a57d7fd47e3089e4a
hoon <dlgjsdn999@naver.com>
2022-08-03 23:24:09 +0900

    33강 메인화면 만들기

---
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.cos.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    //아무것도 안적힐때와 /가 있을때 다 index.jsp로 간다.
+    @GetMapping({"","/"})
+    public String index() {
+        //yml에 /WEB_INF/views에서 index.jsp로 찾아가게 설정해놨음.
+        return "index";
+    }
+}
